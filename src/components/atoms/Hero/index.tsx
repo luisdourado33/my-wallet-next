1a8916165fe5a86910e1819aecd8202b41a28dd8
Luis Dourado <luis_dourado33@hotmail.com>
2023-04-22 16:26:56 -0400

    feat: add Hero atom

---
+++ src/components/atoms/Hero/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {
+  children?: React.ReactNode;
+  bgImage?: any,
+  className?: string;
+}
+
+export default function Hero({
+  children,
+  className,
+  bgImage,
+}: Props) {
+  return (
+    <div className={`px-12 bg-purple-900 ${className}`}>{children}</div>
+  )
+}
