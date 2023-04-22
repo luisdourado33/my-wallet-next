73100b5b7267ac4916ef04a708061d2971c68e62
Luis Dourado <luis_dourado33@hotmail.com>
2023-04-22 14:12:17 -0400

    feat: add authenticationSlice reducers

--- src/features/authentication/authenticationSlice.ts
+++ src/features/authentication/authenticationSlice.ts
@@ -1,6 +1,9 @@
-import { IAuthentication } from '@/@types/authentication.types';
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
+
+import { IAuthentication } from '@/@types/authentication.types';
+import { makeUser } from '@/core/services/mock/authentication';
+import { AccessGroupEnum } from '@/@types/user.types';
 
 const initialState: IAuthentication = {
   isAuthenticated: false
@@ -10,12 +13,19 @@ export const authenticationSlice = createSlice({
   name: 'authentication',
   initialState,
   reducers: {
-    signIn(state) {
-      state.isAuthenticated = false;
+    signIn(state, action: PayloadAction<IAuthentication>) {
+      state = action.payload;
+    },
+    signInMock(state) {
+      state.isAuthenticated = true;
+      state.user = makeUser(AccessGroupEnum.MEMBER);
     },
     signOut(state) {
-      state.isAuthenticated = true,
-        
+      state.isAuthenticated = false;
+      state.user = undefined;
     }
   }
 });
+
+export const { signIn, signInMock, signOut } = authenticationSlice.actions;
+export default authenticationSlice.reducer;
