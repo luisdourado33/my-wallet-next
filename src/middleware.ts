56c5f48a5161e650cfba6cf554e63a48d7e358c0
Luis Dourado <luis_dourado33@hotmail.com>
2023-04-22 13:31:14 -0400

    feat: add first middleware testing

---
+++ src/middleware.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  // return NextResponse.json({ ok: true, message: 'JSON returned from middleware' })
+}
