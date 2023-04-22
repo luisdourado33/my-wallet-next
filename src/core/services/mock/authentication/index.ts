ae54818330b7cd17a5ec55937af6bee7198c7df0
Luis Dourado <luis_dourado33@hotmail.com>
2023-04-22 13:58:12 -0400

    feat(mock): add user factory

---
+++ src/core/services/mock/authentication/index.ts
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker';
+import { AccessGroupEnum, IUser } from "@/@types/user.types";
+
+/**
+ * makeUser works as an User factory
+ * generating an user by its access group
+ * @returns IUser
+ */
+export const makeUser = (accessGroup: AccessGroupEnum): IUser => {
+  return {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    accessGroup: accessGroup,
+    balance: Number(faker.finance.amount()),
+  }
+}
